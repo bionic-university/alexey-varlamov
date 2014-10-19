bb9d5a548a634971477ef0271bc2040040e0015f
Alex <l.e.h.a.vav@gmail.com>
2014-10-19 23:09:36 +0300

    Changes to gitignore & include_path

--- lesson-2/hw/parser.php
+++ lesson-2/hw/parser.php
@@ -3,7 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // set include path
-set_include_path(get_include_path().PATH_SEPARATOR.'app');
+$pwd = dirname(__FILE__);
+set_include_path(get_include_path().PATH_SEPARATOR.$pwd.PATH_SEPARATOR.$pwd.DIRECTORY_SEPARATOR."app");
 
 // include class autoloader
 require_once('lib/Autoloader.php');
