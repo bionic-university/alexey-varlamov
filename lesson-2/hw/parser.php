aaadc1180dd6cc53789ec8085f3defef65d5a9a2
php <l.e.h.a.vav@gmail.com>
2014-10-28 19:37:16 +0200

    Changed include_path

--- lesson-2/hw/parser.php
+++ lesson-2/hw/parser.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // define include path
-$pwd = getcwd();
+$pwd = getcwd().DIRECTORY_SEPARATOR.dirname($_SERVER['SCRIPT_NAME']);
 set_include_path(get_include_path().PATH_SEPARATOR.$pwd.PATH_SEPARATOR.$pwd.DIRECTORY_SEPARATOR."app");
 
 // include class autoloader
