4486ef43d9342d7e39c1b768c1e7aa78860216b9
Alex <l.e.h.a.vav@gmail.com>
2014-12-14 22:50:08 +0200

    Formated header

--- project/src/Vav/CashTarget/template/goal/empty.php
+++ project/src/Vav/CashTarget/template/goal/empty.php
@@ -1,5 +1,7 @@
 <?php
 echo str_repeat(PHP_EOL, 5);
-echo str_repeat(' ', 200) . $this->getHeader() . PHP_EOL . PHP_EOL;
+echo str_repeat(' ', 200);
+$this->output->writeln($this->getHeader());
+echo str_repeat(PHP_EOL, 3);
 echo str_repeat(' ', 150) . $this->getMessage();
 echo str_repeat(PHP_EOL, 5);
